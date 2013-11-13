9264178445d8bc8422515ec3b7eb8c6d921ef417
Frijol <ifoundthemeaningoflife@gmail.com>
2013-11-12 17:46:45 -0800

    jade files ok, need to save req.body in ingredeint.js route to mongodb

--- routes/order.js
+++ routes/order.js
@@ -4,10 +4,14 @@ Ingredient = require('../models/ingredient.js');
 exports.new = function (req, res) {
 	var ingredients = Ingredient.find({}, function (err, docs) {
 		if (err) throw err;
-		res.render('neworder', {ingredients: docs.sort()})
+		res.render('neworder', {ingredients: docs.sort()});
+		console.log(docs.sort())
 	});
 }
 
 exports.orders = function (req, res) {
-	res.render('../views/orders.jade', {});
+	var orders = Order.find({}, function (err, docs) {
+		if (err) throw err;
+		res.render('orders', {orders: docs.sort()});
+	});
 }
